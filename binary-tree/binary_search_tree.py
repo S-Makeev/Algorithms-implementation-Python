4ea0e9280db894c4f1fa2dbfd08e0987c1cc80e8
Slava Makeev <unclearnuclear@yahoo.com>
2023-09-03 05:37:19 +0000

    initial implementation

---
+++ binary-tree/binary_search_tree.py
@@ -0,0 +1,13 @@
+class Node:
+  def __init__(self, data=None):
+    self.data = data
+    self.left_child = None
+    self.right_child = None
+
+class Binary_Search_Tree:
+  def __init__(self):
+    self.root = None
+
+  def insert(self, data):
+      if self.root is None:
+          self.root = Node(data)
